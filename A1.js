23d5f95dca9e42cc1e1a1f94887e9d2efe0b3fe2
Giovanemerotto <giovanemerottodaluz12@gmail.com>
2025-07-29 09:32:06 -0300

    message

---
+++ A1.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let nome =prompt ("escreva o seu nome",nome );
+let idade =prompt ("escreva o sua idade",idade);
+let altura =prompt ("escreva o sua altura",altura);
+console.log("esssas sao as suas informações",nome,altura,idade);
---
+++ aula 0.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")();
+let numeroA = 3
+let numeroB = 7
+console.log (numeroA + numeroB)
