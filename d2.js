6f43270f1c5e6315bc32e112548229226e128d64
Ayatoo <giovanemerottodaluz12@gmail.com>
2025-12-26 00:34:17 -0300

    Delete d2.js

--- d2.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-let idadeAlist = 18;
-let sexo = "M"; // "F" feminino
-if (sexo === "M" && idadeAlist >= 18) {
-  console.log("Alistamento obrigatório");
-} else {
-  console.log("Alistamento não obrigatório");
-}
