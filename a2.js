e7014feae87367620837b31caa8bdd23844059f9
Ayatoo <giovanemerottodaluz12@gmail.com>
2025-12-26 00:29:17 -0300

    Delete a2.js

--- a2.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-let nota1 = 7, nota2 = 6, nota3 = 8;
-let media = (nota1 + nota2 + nota3) / 3;
-if (media >= 7) {
-  console.log("Aprovado com média:", media);
-} else {
-  console.log("Reprovado com média:", media);
-}
