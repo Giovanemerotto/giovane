261bb3e41a2236c3a06fc479f124d722aa4f6822
Ayatoo <giovanemerottodaluz12@gmail.com>
2025-12-26 00:29:25 -0300

    Delete a3.js

--- a3.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-let totalXP = 0;
-
-for (let i = 1; i <= 10; i++) {
-  console.log("Batalha " + i + ": Você ganhou 10 pontos de experiência!");
-  totalXP += 10;
-}
-
-console.log("Total de experiência recebida:", totalXP); 
