8c7466b8ec32fbd1ce557bbd341863aa4d5fc93b
Ayatoo <giovanemerottodaluz12@gmail.com>
2025-12-26 00:39:01 -0300

    Delete i3.js

--- i3.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-let numero = 28;
-
-console.log("Divisores de", numero, ":");
-for (let i = 1; i <= numero; i++) {
-  if (numero % i === 0) {
-    console.log(i);
-  }
-}
