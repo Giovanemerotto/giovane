74b42285f74cfefc691f31033a627a1585bf9a43
Ayatoo <giovanemerottodaluz12@gmail.com>
2025-12-26 00:34:07 -0300

    Delete d1.js

--- d1.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-let preco = 120;
-let desconto = preco * 0.15;
-let precoFinal = preco - desconto;
-precoFinal = precoFinal + 10;
-console.log("Valor final do pedido:", precoFinal);
